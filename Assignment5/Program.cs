ee16aed9bcdb90bbb9fbe4c9236a4961b13c6fe5
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-07 11:05:46 +0300

    Update Program.cs

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine(v);
 //v.MergeSort();
 
 int[] arr = new int[256];
-
+// Якщо Ви хочете передавати масив як параметр, то краще це було пеалізувати як статичний метод сервісного статичного класу.
 v.MergeSortHalfLength(ref arr);
 Vector v2 = new Vector(arr);
 Console.WriteLine(v2);
